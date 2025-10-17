e986b3d27d308cf76e38b718d9a46a14fd33350a
p28Gamaliel <gamaliel13perez@gmail.com>
2025-10-17 20:57:35 -0300

    update of the methods of the facade

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,16 +1,27 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import Repository
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Rewiew
+from app.models.amenity import Amenity
+from app.models.location import Location
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = Repository()
+        self.place_repo = Repository()
+        self.review_repo = Repository()
+        self.amenity_repo = Repository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.User_repo.create(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
